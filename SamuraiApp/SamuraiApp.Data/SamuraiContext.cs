be47a804ec17467dcb6a71ea91c197a42ca3f8f6
Edin Jašarević <edinjasar14@gmail.com>
2020-02-21 23:46:34 +0100

    specifying the data provider and connection string

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -13,5 +13,10 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = SamuraiAppData; Trusted_Connection = True; ");
+        }
     }
 }
