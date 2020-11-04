b8e413c49db2dfe9fe247576ee49e2e50b9d07db
Edin Jašarević <edinjasar14@gmail.com>
2020-11-04 20:57:20 +0100

    sql logs

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
@@ -16,15 +19,21 @@ namespace SamuraiApp.Data
         protected override void OnModelCreating
             (ModelBuilder modelBuilder)
         {
+            
+
             modelBuilder.Entity<SamuraiBattle>()
                 .HasKey(s => new { s.BattleId,
                                    s.SamuraiId });
         }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer(
-        //         "Server = .; Database = SamuraiAppData; Trusted_Connection = True; ");
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder
+                .AddConsole()
+                .AddFilter(level => level >= LogLevel.Information)
+            );
+            var loggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
     }
 }
